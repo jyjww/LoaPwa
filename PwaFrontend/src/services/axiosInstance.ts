bc6e4242e3f1b2717371d08dbaa0030fd9d9e983
jyjww <imabatmanithink@gmail.com>
2025-09-26 18:15:25 +0900

    ci: inject VITE_API_URL into build

--- PwaFrontend/src/services/axiosInstance.ts
+++ PwaFrontend/src/services/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 
-const API = import.meta.env.VITE_API_URL ?? 'http://localhost:4000';
+// const API = import.meta.env.VITE_API_URL ?? 'http://localhost:4000';
+
+const API = (import.meta.env.VITE_API_URL ?? 'http://localhost:4000').replace(/\/+$/, '');
 
 type JwtPayload = { exp: number };
 
